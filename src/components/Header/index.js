89bf6c73335b6d06e58bdf2b60b3e9a8b9da61d4
Öznur Özen <71924410+oznurozen@users.noreply.github.com>
2023-10-16 20:16:38 +0300

    update

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <>
     <header className="header">
-        <h1># -- Yapılacaklar Listesi -- #</h1>
+        <h1>Yapılacaklar Listesi</h1>
        <HeaderForm/>
     </header>
 
